4b65971620653ae6908493152d9a88f4941e1935
derktam <derktam@naver.com>
2016-02-03 10:17:23 +0900

    웹 수정
    ㅇㅇ

--- public/javascripts/websocket.js
+++ public/javascripts/websocket.js
@@ -1,7 +1,7 @@
 /**
  * Created by Minhyeong on 2016-01-24.
  */
-var socket = io.connect("http://127.0.0.1:3000/", {
+var socket = io.connect("http://192.168.100.135:3000/", {
     "connect timeout": 2000,
     "reconnect": true,
     "reconnection delay": 1000,
