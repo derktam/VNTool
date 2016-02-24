5dedc4f5a8b7d5e1c80c50ef7f46ecd713cb9047
derktam <derktam@naver.com>
2016-02-24 13:37:54 +0900

    로그 timestamp 모양 변경
    ㅇㅇ

--- bin/main.js
+++ bin/main.js
@@ -1,7 +1,7 @@
 /**
  * Created by Minhyeong on 2016-01-04.
  */
-require('console-stamp')(console, '[HH:MM:ss.l]');
+require('console-stamp')(console, 'HH:MM:ss');
 
 var www = require('./../app'),
     websocket = require('./websocket')(www.io, this),
